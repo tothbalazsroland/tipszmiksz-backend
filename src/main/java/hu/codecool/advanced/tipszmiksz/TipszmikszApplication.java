691a9f33fc52ea11a0dba023ad3d14cc8b83b830
Toth <toth.balazs.roland@gmail.com>
2019-03-18 13:46:50 +0100

    initial commit

---
+++ src/main/java/hu/codecool/advanced/tipszmiksz/TipszmikszApplication.java
@@ -0,0 +1,13 @@
+package hu.codecool.advanced.tipszmiksz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipszmikszApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipszmikszApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/codecool/advanced/tipszmiksz/TipszmikszApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.codecool.advanced.tipszmiksz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TipszmikszApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
