6a60687e12637d3b383f39c6ec238f89a323e0bb
Toth <toth.balazs.roland@gmail.com>
2019-03-19 10:57:52 +0100

    added index controller

--- src/main/java/hu/codecool/advanced/tipszmiksz/controller/IndexController.java
+++ src/main/java/hu/codecool/advanced/tipszmiksz/controller/IndexController.java
@@ -1,4 +1,23 @@
 package hu.codecool.advanced.tipszmiksz.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+@Slf4j
 public class IndexController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/")
+    public String index(){
+        log.info("Got a get a request");
+        return "I am working as expected";
+    }
+
 }
