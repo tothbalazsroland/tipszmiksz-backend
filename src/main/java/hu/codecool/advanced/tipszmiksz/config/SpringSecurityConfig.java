e93c113e7c7fecc1ea220bcbfd8681e5f5ae0fd7
Toth <toth.balazs.roland@gmail.com>
2019-03-21 10:35:33 +0100

    defined bcryptencoder bean in spring sercurity config

--- src/main/java/hu/codecool/advanced/tipszmiksz/config/SpringSecurityConfig.java
+++ src/main/java/hu/codecool/advanced/tipszmiksz/config/SpringSecurityConfig.java
@@ -1,4 +1,17 @@
 package hu.codecool.advanced.tipszmiksz.config;
 
-public class SpringSecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
