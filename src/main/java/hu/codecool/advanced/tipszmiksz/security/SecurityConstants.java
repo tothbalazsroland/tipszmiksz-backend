1367b8913190875ccbad116e188072d4829a5d6d
Toth <toth.balazs.roland@gmail.com>
2019-03-21 21:22:16 +0100

    created security constants

--- src/main/java/hu/codecool/advanced/tipszmiksz/security/SecurityConstants.java
+++ src/main/java/hu/codecool/advanced/tipszmiksz/security/SecurityConstants.java
@@ -1,4 +1,9 @@
 package hu.codecool.advanced.tipszmiksz.security;
 
 public class SecurityConstants {
+    public static final String SECRET = "balazsesforthewin";
+    public static final long EXPIRATION_TIME = 3_600_000; // 1 hour
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
 }
+
