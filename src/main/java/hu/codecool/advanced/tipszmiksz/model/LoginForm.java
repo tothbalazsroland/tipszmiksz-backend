4653d4311825be35993bec47888d82ddf2437aa0
Toth <toth.balazs.roland@gmail.com>
2019-03-21 21:21:47 +0100

    created loginform

--- src/main/java/hu/codecool/advanced/tipszmiksz/model/LoginForm.java
+++ src/main/java/hu/codecool/advanced/tipszmiksz/model/LoginForm.java
@@ -1,4 +1,15 @@
 package hu.codecool.advanced.tipszmiksz.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginForm {
+
+    private String email;
+    private String password;
+
 }
